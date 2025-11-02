d1a56bcdc4a56e4e7f921da85589ed1080c93f9d
nutname041-cpu <nutname041@gmail.com>
2025-11-02 22:14:18 +0530

    Refactor handler to return clean message on GET

--- api/index.js
+++ api/index.js
@@ -1,13 +1,18 @@
 export default async function handler(req, res) {
-  const TARGET = process.env.TENDERLY_RPC || "https://virtual.mainnet.eu.rpc.tenderly.co/6634dcc3-e5ef-48d3-a21b-328a3cfdb6c6";
+  // If someone just opens the link in a browser (GET), show a clean message
+  if (req.method === "GET") {
+    return res.status(200).send("!");
+  }
+
+  const TARGET =
+    process.env.TENDERLY_RPC ||
+    "https://virtual.mainnet.eu.rpc.tenderly.co/6634dcc3-e5ef-48d3-a21b-328a3cfdb6c6";
 
   try {
     const response = await fetch(TARGET, {
       method: req.method,
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(req.body)
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(req.body),
     });
 
     const text = await response.text();
@@ -18,3 +23,4 @@ export default async function handler(req, res) {
     res.status(500).json({ error: "Proxy error", message: err.message });
   }
 }
+
